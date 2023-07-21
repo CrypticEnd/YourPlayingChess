4cdda24d59086b1c6f02c04c58629b090bea52da
Devon Reece <devon.reece@fdmgroup.com>
2023-07-22 10:29:57 +1200

    User name checking added

--- YPC backend/src/main/java/com/cryptic/ypc/service/UserService.java
+++ YPC backend/src/main/java/com/cryptic/ypc/service/UserService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import com.cryptic.ypc.dal.RegisteredUserRepository;
 import com.cryptic.ypc.dal.UserRepository;
+import com.cryptic.ypc.exceptions.BadRequestException;
+import com.cryptic.ypc.model.user.User;
 
 @Service
 public class UserService {
@@ -21,4 +23,27 @@ public class UserService {
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	
+
+	/**
+	 * Checks user name for correct format. Auto Trims username as trailing blank
+	 * spaces are not allowed Throws bad request with why it failed
+	 * 
+	 * @param username user name to check
+	 */
+	private void checkUserName(String username) {
+		username = username.trim();
+		int nameLen = username.length();
+
+		if (nameLen < 4 || nameLen >= 15) {
+			logger.debug(String.format("Username: '%s' failed to be correct length", username));
+			throw new BadRequestException("User name must be between 4 and 15 characters");
+		}
+
+		// Checks if user name is only letters dashes and underscores
+		if (!username.matches("[a-zA-Z0-9\\-\\_]+")) {
+			logger.debug(String.format("Username: '%s', contains not allowed chars", username));
+			throw new BadRequestException("User name can only contain letters, dashes [-] or underscores [_]");
+		}
+	}
+
 }
