826574a969fe38419b6c6af09d84bd9656295be4
Devon Reece <devon.reece@fdmgroup.com>
2023-07-22 09:55:23 +1200

    Decleared some varbiles for user service

--- YPC backend/src/main/java/com/cryptic/ypc/service/UserService.java
+++ YPC backend/src/main/java/com/cryptic/ypc/service/UserService.java
@@ -1,10 +1,24 @@
 package com.cryptic.ypc.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.cryptic.ypc.dal.RegisteredUserRepository;
+import com.cryptic.ypc.dal.UserRepository;
 
 @Service
 public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private RegisteredUserRepository registeredUserRepository;
+	@Autowired
+	private PasswordEncoder encoder;
 	
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	
 }
