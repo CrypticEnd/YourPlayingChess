c2fe5be521a5ac4cfd33f674223e7f33a31e7e32
Devon Reece <devon.reece@fdmgroup.com>
2023-07-22 11:26:41 +1200

    Created user controller with save user endpoints

---
+++ YPC backend/src/main/java/com/cryptic/ypc/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.cryptic.ypc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cryptic.ypc.model.user.GuestUser;
+import com.cryptic.ypc.model.user.RegisteredUser;
+import com.cryptic.ypc.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/registered")
+	@ResponseStatus(HttpStatus.CREATED)
+	public long saveNewRegisteredUser(@RequestBody RegisteredUser user) {
+		return this.userService.save(user);
+	}
+	
+	@PostMapping("/guest")
+	@ResponseStatus(HttpStatus.CREATED)
+	public long saveNewGuestUser(@RequestBody GuestUser user) {
+		return this.userService.save(user);
+	}
+	
+	
+}
