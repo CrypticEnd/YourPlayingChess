94ec964a315603a8c48e37b365952868d93ac7c1
Devon Reece <devon.reece@fdmgroup.com>
2023-07-22 09:52:26 +1200

    Added a updateGuestUserToRegistered. From prevois project

--- YPC backend/src/main/java/com/cryptic/ypc/dal/UserRepository.java
+++ YPC backend/src/main/java/com/cryptic/ypc/dal/UserRepository.java
@@ -1,9 +1,20 @@
 package com.cryptic.ypc.dal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cryptic.ypc.model.user.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 	public boolean existsById(int id);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "UPDATE users SET type = 'Registered', user_name = :name, password = :password "
+			+ " WHERE user_id = :id", nativeQuery = true)
+	public void updateGuestUserToRegistered(@Param("id") Long id, @Param("name") String username,
+			@Param("password") String password);
 }
