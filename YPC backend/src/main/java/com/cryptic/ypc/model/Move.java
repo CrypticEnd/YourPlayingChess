223cf918c4ab740da36d9b732b77cabba5b23940
Devon Reece <devon.reece@fdmgroup.com>
2023-07-22 13:18:30 +1200

    renamed gamemove to move

--- YPC backend/src/main/java/com/cryptic/ypc/model/Move.java
+++ YPC backend/src/main/java/com/cryptic/ypc/model/Move.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class GameMove {
+public class Move {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "move_id")
