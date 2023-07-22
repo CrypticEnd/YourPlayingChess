ed9dc4ea3d915b2dccbe4152f159c26964b8bfc4
Devon Reece <devon.reece@fdmgroup.com>
2023-07-22 13:38:35 +1200

    move and game repo

---
+++ YPC backend/src/main/java/com/cryptic/ypc/dal/GameRepository.java
@@ -0,0 +1,11 @@
+package com.cryptic.ypc.dal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cryptic.ypc.model.Game;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Integer>{
+
+}
---
+++ YPC backend/src/main/java/com/cryptic/ypc/dal/MoveRepository.java
@@ -0,0 +1,11 @@
+package com.cryptic.ypc.dal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cryptic.ypc.model.Move;
+
+@Repository
+public interface MoveRepository extends JpaRepository<Move, Long>{
+	
+}
