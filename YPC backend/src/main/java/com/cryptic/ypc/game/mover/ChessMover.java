c81c73705c4e1245d9e53d81a06bd4ed9f0a487d
Devon Reece <devon.reece@fdmgroup.com>
2023-07-23 22:09:05 +1200

    Added methods to IchessMover

--- YPC backend/src/main/java/com/cryptic/ypc/game/mover/ChessMover.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/mover/ChessMover.java
@@ -1,5 +1,54 @@
 package com.cryptic.ypc.game.mover;
 
+import java.util.List;
+
+import com.cryptic.ypc.game.BoardChange;
+import com.cryptic.ypc.game.BoardState;
+import com.cryptic.ypc.game.piece.chess.Bishop;
+import com.cryptic.ypc.game.piece.chess.King;
+import com.cryptic.ypc.game.piece.chess.Knight;
+import com.cryptic.ypc.game.piece.chess.Pawn;
+import com.cryptic.ypc.game.piece.chess.Queen;
+import com.cryptic.ypc.game.piece.chess.Rook;
+
 public class ChessMover implements IChessMover{
+	private boolean canTakePiece = false;
+	
+
+	@Override
+	public List<BoardChange> moveBishop(BoardState boardState, Bishop bishop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardChange> moveKing(BoardState boardState, King king) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardChange> moveKnight(BoardState boardState, Knight knight) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardChange> movePawn(BoardState boardState, Pawn pawn) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardChange> moveQueen(BoardState boardState, Queen queen) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardChange> moveRook(BoardState boardState, Rook rook) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- YPC backend/src/main/java/com/cryptic/ypc/game/mover/IChessMover.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/mover/IChessMover.java
@@ -1,5 +1,35 @@
 package com.cryptic.ypc.game.mover;
 
-public interface IChessMover extends IMover {
+import java.util.List;
 
+import com.cryptic.ypc.game.BoardChange;
+import com.cryptic.ypc.game.BoardState;
+import com.cryptic.ypc.game.piece.chess.Bishop;
+import com.cryptic.ypc.game.piece.chess.King;
+import com.cryptic.ypc.game.piece.chess.Knight;
+import com.cryptic.ypc.game.piece.chess.Pawn;
+import com.cryptic.ypc.game.piece.chess.Queen;
+import com.cryptic.ypc.game.piece.chess.Rook;
+
+/**
+ * This uses the Visitor pattern For each chess piece has its own move method
+ * that is on for the concrete implementation Returns a list of board changes
+ * It's own move is one change, but concrete implementation could have removing
+ * a piece it landed on or changing it into a another piece
+ * 
+ * @author reece
+ *
+ */
+public interface IChessMover extends IMover {
+	public List<BoardChange> moveBishop(BoardState boardState, Bishop bishop);
+
+	public List<BoardChange> moveKing(BoardState boardState, King king);
+
+	public List<BoardChange> moveKnight(BoardState boardState, Knight knight);
+
+	public List<BoardChange> movePawn(BoardState boardState, Pawn pawn);
+
+	public List<BoardChange> moveQueen(BoardState boardState, Queen queen);
+
+	public List<BoardChange> moveRook(BoardState boardState, Rook rook);
 }
