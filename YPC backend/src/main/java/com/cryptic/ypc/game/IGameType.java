34e1d88f774c9e25c4fc40b6980e399e09b48c54
Devon Reece <devon.reece@fdmgroup.com>
2023-07-24 08:21:19 +1200

    Added more methods to IGameType to repecnet changes in how board changes are done

--- YPC backend/src/main/java/com/cryptic/ypc/game/IGameType.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/IGameType.java
@@ -1,20 +1,30 @@
 package com.cryptic.ypc.game;
 
+import com.cryptic.ypc.game.mover.IMover;
 import com.cryptic.ypc.model.Move;
 import com.cryptic.ypc.model.enums.GameType;
+import com.cryptic.ypc.model.enums.Player;
 
+/**
+ * Game type implementation in most cases could be a singleton.
+ * It needs to store:
+ * 		- default board state, 
+ * 		- Imover class concrete implementation
+ * 		- A game type identifier 
+ * 
+ * 		Main functionality is to convert a single board change into many moves
+ * 		uses the IMover 
+ * 
+ * @author reece
+ *
+ */
 public interface IGameType {
-
-	public BoardState getBoardState();
-
+	public BoardState getDefaultBoardState();
 	public GameType getType();
+	public IMover getIMover(IMover iMover);
 
-	/**
-	 * @param boardState
-	 * @param move       The same as the ypc.model.move.move. Char is two bytes long
-	 *                   and expected to be in same format
-	 * @return
-	 */
-	public Move makeMove(BoardState boardState, char move);
+	public Move makeMove(BoardState boardState, BoardChange move);
 
+
+	
 }
