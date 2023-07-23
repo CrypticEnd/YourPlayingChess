957ae77e057b138d8825cc7b0da7117164fc81a6
Devon Reece <devon.reece@fdmgroup.com>
2023-07-24 08:34:01 +1200

    changed board peice from interface to abstract class

--- YPC backend/src/main/java/com/cryptic/ypc/game/IBoardPiece.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/IBoardPiece.java
@@ -1,9 +1,16 @@
 package com.cryptic.ypc.game;
 
+import java.util.List;
+
 import com.cryptic.ypc.game.mover.IMover;
+import com.cryptic.ypc.model.enums.Player;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
- * Board piece interface.
+ * Board piece abstract class .
  * Expects all children to have an ID that in Unique across the game.
  * Each board piece is only needed once so should use singleton design 
  * 
@@ -16,9 +23,14 @@ import com.cryptic.ypc.game.mover.IMover;
  * @author reece
  *
  */
-public interface IBoardPiece {
-
-	public Byte getId();
-	public String getName();
-	boolean move(IMover mover, BoardState boardState, BoardChange move);
+@Getter
+@Setter
+@ToString
+public abstract class IBoardPiece {
+	protected Player player;
+	protected String name;
+	
+	public abstract byte getId();
+	
+	public abstract List<BoardChange> move(IMover mover, BoardState boardState, BoardChange move);
 }
