7325ea09b86d165680b42ba2ec54a1c4df29dfb6
Devon Reece <devon.reece@fdmgroup.com>
2023-07-23 22:09:17 +1200

    Added methods to IConnectFourMover

--- YPC backend/src/main/java/com/cryptic/ypc/game/mover/ConnectFourMover.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/mover/ConnectFourMover.java
@@ -1,5 +1,23 @@
 package com.cryptic.ypc.game.mover;
 
+import java.util.List;
+
+import com.cryptic.ypc.game.BoardChange;
+import com.cryptic.ypc.game.BoardState;
+import com.cryptic.ypc.game.piece.ConnectFourToken;
+
 public class ConnectFourMover implements IConnectFourMover{
+
+	@Override
+	public List<BoardChange> placeToken(BoardState boardState, ConnectFourToken token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardChange> tokenFall(BoardState boardState, ConnectFourToken token) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- YPC backend/src/main/java/com/cryptic/ypc/game/mover/IConnectFourMover.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/mover/IConnectFourMover.java
@@ -1,5 +1,31 @@
 package com.cryptic.ypc.game.mover;
 
-public interface IConnectFourMover extends IMover{
+import java.util.List;
+
+import com.cryptic.ypc.game.BoardChange;
+import com.cryptic.ypc.game.BoardState;
+import com.cryptic.ypc.game.piece.ConnectFourToken;
+
+/**
+ * This uses the Visitor pattern Returns a list of board changes It's own move
+ * is one change, but concrete implementation could have removing a piece it
+ * landed on or changing or having a cascading effect
+ * 
+ * @author reece
+ *
+ */
+public interface IConnectFourMover extends IMover {
+
+	public List<BoardChange> placeToken(BoardState boardState, ConnectFourToken token);
+
+	/**
+	 * For if the token can/cannot fall. Connect four tends to have tokens falling
+	 * but concrete implementation could change that
+	 * 
+	 * @param boardState
+	 * @param token
+	 * @return
+	 */
+	public List<BoardChange> tokenFall(BoardState boardState, ConnectFourToken token);
 
 }
