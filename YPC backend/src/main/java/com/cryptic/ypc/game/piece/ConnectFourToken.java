a8bc7194a04b9c4ff76f76cb037bc401f6e59ade
Devon Reece <devon.reece@fdmgroup.com>
2023-07-23 20:11:20 +1200

    Connect four peice

---
+++ YPC backend/src/main/java/com/cryptic/ypc/game/piece/ConnectFourToken.java
@@ -0,0 +1,41 @@
+package com.cryptic.ypc.game.piece;
+
+import com.cryptic.ypc.game.BoardState;
+import com.cryptic.ypc.game.IBoardPiece;
+import com.cryptic.ypc.game.moveValidation.IMoveValidator;
+
+import lombok.ToString;
+
+@ToString
+public final class ConnectFourToken implements IBoardPiece {
+	private static ConnectFourToken INSTANCE;
+	private final byte id = 102;
+	private static final String name = "Token";
+
+	private ConnectFourToken() {
+	}
+
+	public static ConnectFourToken geToken() {
+		if (INSTANCE == null) {
+			INSTANCE = new ConnectFourToken();
+		}
+
+		return INSTANCE;
+	}
+
+	@Override
+	public Byte getId() {
+		return id;
+	}
+
+	@Override
+	public boolean canMakeMove(IMoveValidator validator, BoardState boardState, char move) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+}
