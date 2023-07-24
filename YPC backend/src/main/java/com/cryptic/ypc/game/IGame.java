5a525044a2d338224096b70bf0824d495359d9e8
Devon Reece <devon.reece@fdmgroup.com>
2023-07-24 20:01:54 +1200

    Updated IGame due to enum deletion

--- YPC backend/src/main/java/com/cryptic/ypc/game/IGame.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/IGame.java
@@ -1,14 +1,12 @@
 package com.cryptic.ypc.game;
 
+import com.cryptic.ypc.model.GameType;
 import com.cryptic.ypc.model.Move;
-import com.cryptic.ypc.model.enums.GameType;
 import com.cryptic.ypc.model.enums.Player;
 
 /**
- * Game implementation in most cases should be a singleton. It needs to store:
- * 		default board state
- * 		Imover class concrete implementation 
- * 		A game type identifier
+ * Game implementation in most cases should be a singleton. It needs to store a
+ * gametype, IMover and rules
  * 
  * Main functionality is to convert a single board change into many moves uses
  * the IMover
@@ -19,7 +17,7 @@ import com.cryptic.ypc.model.enums.Player;
 public interface IGame {
 	public BoardState getDefaultBoardState();
 
-	public GameType getType();
+	public GameType getGameType();
 
 	public Move makeMove(String boardState, BoardChange move, Player playerTurn);
 }
