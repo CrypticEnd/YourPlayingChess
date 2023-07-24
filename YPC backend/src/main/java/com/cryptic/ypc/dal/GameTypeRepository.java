73e11b6c650a32621fd867cc511b0ccd4163534b
Devon Reece <devon.reece@fdmgroup.com>
2023-07-25 10:12:53 +1200

    Created a gametype repo and a gameservice with a TODO reminder

---
+++ YPC backend/src/main/java/com/cryptic/ypc/dal/GameTypeRepository.java
@@ -0,0 +1,11 @@
+package com.cryptic.ypc.dal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cryptic.ypc.model.GameType;
+
+@Repository
+public interface GameTypeRepository extends JpaRepository<GameType, Byte>{
+
+}
---
+++ YPC backend/src/main/java/com/cryptic/ypc/service/GameService.java
@@ -0,0 +1,16 @@
+package com.cryptic.ypc.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.cryptic.ypc.game.rule.IGameRule;
+
+@Service
+public class GameService {
+	//TODO initialize this list by getting all game types as a hash map <GameType, IGameRUle>
+	List<IGameRule> gameRules;
+	
+	
+	
+}
