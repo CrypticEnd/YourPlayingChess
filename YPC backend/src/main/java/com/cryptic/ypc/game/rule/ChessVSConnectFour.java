d99d00b69697393da91614ce92ef333a354203d7
Devon Reece <devon.reece@fdmgroup.com>
2023-07-25 10:54:33 +1200

    Some implemation of ChessVsConnectFour rules

--- YPC backend/src/main/java/com/cryptic/ypc/game/rule/ChessVSConnectFour.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/rule/ChessVSConnectFour.java
@@ -1,5 +1,7 @@
 package com.cryptic.ypc.game.rule;
 
+import org.springframework.data.repository.query.ParameterOutOfBoundsException;
+
 import com.cryptic.ypc.game.BoardChange;
 import com.cryptic.ypc.game.BoardState;
 import com.cryptic.ypc.model.GameType;
@@ -8,6 +10,14 @@ import com.cryptic.ypc.model.enums.Player;
 
 public final class ChessVSConnectFour implements IGameRule {
 	private ChessVSConnectFour INSTANCE;
+	/**
+	 * Max amount of players in this game rule system (inclusive)
+	 */
+	private final static int playerMaxAmount = 2;
+	/**
+	 * Min amount of player in this game rule system (inclusive)
+	 */
+	private final static int playerMinAmount = 2;
 
 	private ChessVSConnectFour() {
 	}
@@ -23,6 +33,49 @@ public final class ChessVSConnectFour implements IGameRule {
 	public Move makeMove(String boardState, BoardChange move, Player playerTurn) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public Player calculatePlayerTurn(Player whoWentFirst, int amountOfPlayers, int AmountOfTurns) {
+		if (whoWentFirst == Player.NONE)
+			return Player.NONE;
+
+		// If game cannot start it cannot be a players turn
+		if (!this.canStartGame(amountOfPlayers))
+			return Player.NONE;
+
+		// Expects only two players so only player one and player two can play
+		if (whoWentFirst != Player.ONE || whoWentFirst != Player.TWO)
+			return Player.NONE;
+
+		// If amount of turns is even whoever went first goes
+		if (AmountOfTurns % 2 == 0) {
+			return whoWentFirst;
+		} else {
+			// Otherwise its the player who did not go first
+			if (whoWentFirst == Player.ONE)
+				return Player.TWO;
+			else {
+				return Player.ONE;
+			}
+		}
+	}
+
+	/**
+	 * Checks if a player can be added to a game based on player max amount
+	 */
+	@Override
+	public boolean canAddPlayer(int amountOfCurrentPlayers) {
+		return amountOfCurrentPlayers < playerMaxAmount;
+	}
+
+	/**
+	 * Checks that a game can start. If it meets the min amount of players and max
+	 * amount of players
+	 */
+	@Override
+	public boolean canStartGame(int amountOfCurrentPlayers) {
+		return amountOfCurrentPlayers >= playerMinAmount && amountOfCurrentPlayers <= playerMaxAmount;
 	}
 
 }
