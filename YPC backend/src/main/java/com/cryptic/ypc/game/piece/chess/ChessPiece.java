6307dc5bfaaf0b76e285339730d2814198810d83
Devon Reece <devon.reece@fdmgroup.com>
2023-07-25 09:11:48 +1200

    Created an abstract chess peice that extends board peice. That checks if Imover is of correct type

---
+++ YPC backend/src/main/java/com/cryptic/ypc/game/piece/chess/ChessPiece.java
@@ -0,0 +1,31 @@
+package com.cryptic.ypc.game.piece.chess;
+
+import com.cryptic.ypc.game.BoardPiece;
+import com.cryptic.ypc.game.mover.IChessMover;
+import com.cryptic.ypc.game.mover.IMover;
+import com.cryptic.ypc.model.enums.Player;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public abstract class ChessPiece extends BoardPiece {
+
+	public ChessPiece(Player player) {
+		super(player);
+	}
+
+	/**
+	 * * A method for children of chess piece Checks if passed IMover is of correct
+	 * type for chess. Throws expection if not
+	 * 
+	 * @param mover
+	 * @return
+	 * @throws IllegalArgumentException If Imover is not a Ichess mover or a child of
+	 */
+	protected IChessMover convertIMoverOrThrow(IMover mover) throws IllegalArgumentException {
+		if (mover instanceof IChessMover)
+			return (IChessMover) mover;
+
+		throw new IllegalArgumentException("Chess piece cannot move with a non chess piece mover");
+	}
+}
