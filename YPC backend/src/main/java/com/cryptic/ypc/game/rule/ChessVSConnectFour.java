652c45dc29fc5f146d0590b498fa23444c4c9f56
Devon Reece <devon.reece@fdmgroup.com>
2023-07-25 17:55:21 +1200

    some checks for connect four player win and logging

--- YPC backend/src/main/java/com/cryptic/ypc/game/rule/ChessVSConnectFour.java
+++ YPC backend/src/main/java/com/cryptic/ypc/game/rule/ChessVSConnectFour.java
@@ -3,6 +3,9 @@ package com.cryptic.ypc.game.rule;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.cryptic.ypc.game.BoardChange;
 import com.cryptic.ypc.game.BoardState;
 import com.cryptic.ypc.game.mover.ChessVSConnectFourMover;
@@ -24,7 +27,13 @@ public final class ChessVSConnectFour implements IGameRule {
 	 * Min amount of player in this game rule system (inclusive)
 	 */
 	private final static int playerMinAmount = 2;
+	/**
+	 * Here incase wanting to change the rules of connect four
+	 */
+	private final static int tokensInARowToWin = 4;
 	private final IMover mover = new ChessVSConnectFourMover();
+	
+	private static Logger logger = LoggerFactory.getLogger(ChessVSConnectFour.class);
 
 	private ChessVSConnectFour() {
 	}
@@ -148,22 +157,67 @@ public final class ChessVSConnectFour implements IGameRule {
 	public Player checkGameWinner(BoardState boardState) {
 		BoardPiece[][] boardPieces = boardState.getAllBoardPiecesWithPostions();
 		int lastArrayIndex = boardPieces.length - 1;
+		
+		logger.debug("---Checking if Chess VS Connect Four Has Wiiner---");
 
 		// ------Connect four win condition------
-		// TODO check for connect four
+		logger.debug("If connect four has a winner");
+		
+		// From top left to top right check each position
+		for (int y = 0; y < boardPieces.length; y++) {
+			int horizontalCounter = 0;
+
+			for (int x = 0; x < boardPieces.length; x++) {
+
+				BoardPiece tmp = boardPieces[x][y];
+
+				// If not connect four
+				if (!this.checkIfPieceIsConnectFourToken(tmp)) {
+					continue;
+				}
+				
+				horizontalCounter++;
+				
+				logger.debug(String.format("Token piece found at [%d/%d] horizontalCounter: %d", x, y, horizontalCounter));
+
+				// Won from horizontal
+				if (horizontalCounter == tokensInARowToWin) {
+					logger.debug("Player two won from horizonal");
+					return Player.TWO;
+				}
+
+				if (checkDownwardsHorizontal(boardPieces, x, y)) {
+					logger.debug("Player two won from horizontal");
+					return Player.TWO;
+				}
+
+			}
+		}
 
 		// ------Connect four lose condition------
-
+		logger.debug("If player one has won");
 		// For each board title in the last row
 		// Check if there is a spot for player two to go
 		for (int i = 0; i < boardPieces[lastArrayIndex].length; i++) {
 			if (boardPieces[lastArrayIndex][i] == null) {
+				logger.debug(String.format("Connect four player can still make a move at [%d/%d]", i, lastArrayIndex));
 				return Player.NONE;
 			}
 		}
 
 		// Player one wins because player two didn't win and cannot move
+		logger.debug("Player one won");
 		return Player.ONE;
+	}
+
+	protected boolean checkDownwardsHorizontal(BoardPiece[][] boardPieces, int x, int y) {
+
+		return false;
+
+	}
+
+	protected boolean checkIfPieceIsConnectFourToken(BoardPiece piece) {
+		return piece instanceof ConnectFourToken;
 	}
 
 }
